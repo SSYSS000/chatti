853eef3ba80deff11b8a04eaaef70b31b958c4ca
SSYSS000 <jaessys@protonmail.com>
2023-03-05 16:27:58 +0200

    initial commit

---
+++ network.c
@@ -0,0 +1,214 @@
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <assert.h>
+
+#include <sys/socket.h>
+
+#include "network.h"
+
+struct net_message *net_message_new(void)
+{
+    struct net_message *ptr = malloc(sizeof(*ptr));
+
+    if (ptr) {
+        ptr->ref_count = 1;
+    }
+
+    return ptr;
+}
+
+struct net_message *net_message_ref(struct net_message *msg)
+{
+    ++msg->ref_count;
+    return msg;
+}
+
+void net_message_unref(struct net_message *msg)
+{
+    assert(msg->ref_count > 0);
+
+    if (--msg->ref_count == 0) {
+        free(msg);
+    }
+}
+
+struct net_endpoint *net_endpoint_new(int fd)
+{
+    struct net_endpoint *endpoint = calloc(1, sizeof(*endpoint));
+    
+    if (endpoint) {
+        endpoint->fd = fd;
+    }
+
+    return endpoint;
+}
+
+void net_endpoint_destroy(struct net_endpoint *endpoint)
+{
+    free(endpoint);
+}
+
+int net_enqueue_message(struct net_endpoint *endpoint, struct net_message *msg)
+{
+    if (endpoint->send_queue_count == NET_ENDP_SEND_QUEUE_SIZE) {
+        return -1;
+    }
+
+    net_message_ref(msg);
+    endpoint->send_queue[endpoint->send_queue_count++] = msg;
+    return endpoint->send_queue_count;
+}
+
+static unsigned net_message_length(const struct net_message *msg)
+{
+    unsigned len;
+    len = (msg->data[0] & 0xffu) << 8 | msg->data[1] & 0xffu;
+    return len;
+}
+
+void net_message_set_body_length(struct net_message *msg, unsigned len)
+{
+    len += NET_MSG_HEADER_LEN;
+    msg->data[0] = (len >> 8) & 0xffu;
+    msg->data[1] = len & 0xffu;
+}
+
+static int net_process_one_send(
+        struct net_endpoint *node,
+        struct net_message *msg,
+        unsigned *num_sent)
+{
+    uint16_t msg_len;
+    ssize_t n;
+
+    msg_len = net_message_length(msg);
+
+    while (*num_sent < msg_len) {
+        n = send(node->fd, msg->data + *num_sent, msg_len - *num_sent, 0);
+        if (n <= 0) {
+            break;
+        }
+
+        *num_sent += n;
+    }
+
+    return n == -1 ? -errno : 0;
+}
+
+int net_process_send(struct net_endpoint *endp)
+{
+    struct net_message *msg;
+    unsigned n_processed;
+    int err;
+
+    for (n_processed = 0; n_processed < endp->send_queue_count; ++n_processed) {
+        msg = endp->send_queue[n_processed];
+        err = net_process_one_send(endpoint, msg, &endp->num_bytes_sent);
+
+        if (err == 0) { 
+            /* Message sent fully. */
+            net_message_unref(msg);
+            endp->num_bytes_sent = 0;
+        }
+        else {
+            break;
+        }
+    }
+
+    /* Shift the queue. */
+    endp->send_queue_count -= n_processed;
+    if (endp->send_queue_count > 0 && n_processed > 0) {
+        memmove(endp->send_queue, endp->send_queue + n_processed,
+                endp->send_queue_count * sizeof(*endp->send_queue));
+    }
+
+    if (err < 0 && err != EWOULDBLOCK && err != EAGAIN) {
+        /* An error. */
+        return err;
+    }
+
+    return endpoint->send_queue_count;
+}
+
+static int net_process_one_receive(
+        struct net_endpoint *endp,
+        struct net_message *msg,
+        unsigned *num_received)
+{
+    unsigned needed;
+
+    if (*num_received < NET_MSG_HEADER_LEN) {
+        /* Need to receive header first. */
+        needed = NET_MSG_HEADER_LEN;
+    }
+    else {
+        needed = net_message_len(msg);
+    }
+
+    while (*num_received < needed) {
+        n = recv(endp->fd, &msg->data[*num_received], needed - *num_received, 0);
+        if (n <= 0)
+            break;
+
+        *num_received += n;
+
+        if (*num_received == NET_MSG_HEADER_LEN) {
+            needed = net_message_len(msg);
+        }
+    }
+
+    return n == -1 ? -errno : n;
+}
+
+int net_process_receive(struct net_endpoint *endp)
+{
+    /* TODO: Finish this function. */
+    struct message *msg;
+
+    /* Get receive buffer. */
+    msg = endp->receive_msg;
+    if (!msg) {
+        msg = endp->receive_msg = net_message_new();
+        if (!msg) {
+            return -ENOMEM;
+        }
+    }
+
+    while (1) {
+        err = net_process_one_receive(endp, msg, &endp->num_bytes_received);
+        if (err > 0) {
+            /* Message received fully. */
+            net_message_unref(msg);
+            endp->num_bytes_received = 0;
+        }
+        else {
+            break;
+        }
+    }
+
+    if (err == -1 && errno != EWOULDBLOCK && errno != EAGAIN) {
+        return -1;
+    }
+
+    return endp->receive_queue_count;
+}
+
+struct net_message *net_receive(struct net_endpoint *endpoint)
+{
+    struct net_message *msg = NULL;
+
+    if (endpoint->receive_queue_count > 0) {
+        msg = endpoint->receive_queue[0];
+
+        /* Shift the queue. */
+        endp->receive_queue_count--;
+        if (endp->receive_queue_count > 0) {
+            memmove(endp->receive_queue, endp->receive_queue + 1,
+                    endp->receive_count * sizeof(*endp->receive_queue));
+        }
+    }
+
+    return msg;
+}
---
+++ network.h
@@ -0,0 +1,104 @@
+#ifndef NETWORK_H
+#define NETWORK_H
+
+#define NET_MSG_DATA_SIZE                       2048u
+#define NET_MSG_LEN_DATA_SIZE                   2u
+#define NET_MSG_HEADER_LEN                      NET_MSG_LEN_DATA_SIZE
+#define NET_ENDP_SEND_QUEUE_SIZE                8u
+#define NET_ENDP_RECEIVE_QUEUE_SIZE             8u
+
+struct net_message {
+    unsigned ref_count;
+    unsigned char data[NET_MSG_DATA_SIZE];
+};
+
+struct net_endpoint {
+    void *identifier;
+    int fd;
+    unsigned num_bytes_sent;
+    unsigned num_bytes_received;
+    unsigned send_queue_count;
+    unsigned receive_queue_count;
+    struct net_message *receive_msg;
+    struct net_message *send_queue[NET_ENDP_SEND_QUEUE_SIZE];
+    struct net_message *receive_queue[NET_ENDP_RECEIVE_QUEUE_SIZE];
+};
+
+struct net_endpoint *net_endpoint_new(int fd);
+
+void net_endpoint_destroy(struct net_endpoint *endpoint);
+
+/**
+ * @brief Allocate memory for a network message buffer.
+ *
+ * @return Network message of ref count 1 or NULL on memory allocation error.
+ */
+struct net_message *net_message_new(void);
+
+/**
+ * @brief Increment the reference count of a network message.
+ *
+ * @param msg Network message.
+ *
+ * @return msg
+ */
+struct net_message *net_message_ref(struct net_message *msg);
+
+/**
+ * @brief Decrement the reference count of a network message,
+ *        possibly freeing it.
+ *
+ * @note If the reference count reaches 0, further usage of msg is not allowed.
+ *
+ * @param msg Network message.
+ */
+void net_message_unref(struct net_message *msg);
+
+/**
+ * @brief Set the body length of the network message (length without header).
+ *
+ * @param msg Network message.
+ * @param len Body length.
+ */
+void net_message_set_body_length(struct net_message *msg, unsigned len);
+
+/**
+ * @brief Enqueue a network message to be sent to an endpoint.
+ *
+ * @param endpoint Endpoint.
+ * @param msg Valid positive length network message.
+ *
+ * @return On success, the new queue length. On error, -1.
+ */
+int net_enqueue_message(struct net_endpoint *endpoint, struct net_message *msg);
+
+/**
+ * @brief Send as much queued data as possible to an endpoint.
+ *
+ * @param endpoint Endpoint.
+ *
+ * @return Send queue length or a negative errno if an error occurred.
+ */
+int net_process_send(struct net_endpoint *endpoint);
+
+
+/**
+ * @brief 
+ *
+ * @param endpoint
+ *
+ * @return 
+ */
+int net_process_receive(struct net_endpoint *endpoint);
+
+
+/**
+ * @brief Get a network message from the receive queue.
+ *
+ * @param endpoint Endpoint.
+ *
+ * @return Network message or NULL if queue is empty.
+ */
+struct net_message *net_receive(struct net_endpoint *endpoint);
+
+#endif /* NETWORK_H */
